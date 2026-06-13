bcfc6ad42be1245cf4a9826cf08d0c306d5bbd93
Hoppelite <hoppelite@gmail.com>
2026-06-14 10:42:26 +1200

    Add entity spawner

--- entities/entity_spawner.gd
+++ entities/entity_spawner.gd
@@ -3,14 +3,14 @@ extends Node2D
 signal spawn_on_position(position, type)
 
 @export var spawn_location: Node2D
-@export var spawnable: Node2D
+@export var spawnable: PackedScene
 
 func _on_static_body_2d_input_event(viewport: Viewport, event: InputEvent, shape_id: int) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			print('spawning human at ', global_position)
-			var enemy = spawnable.duplicate()
-			spawn_location.add_child(enemy)
+			var enemy = spawnable.instantiate()
+			spawn_location.add_child(enemy)		
 			
 			spawn_on_position.emit(global_position, "human")
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
