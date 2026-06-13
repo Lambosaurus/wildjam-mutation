4bc35df75c5311bcc63f924a1b85008f96a8f6f3
Hoppelite <hoppelite@gmail.com>
2026-06-14 10:31:38 +1200

    Switch entity spawner to use resources

--- entities/entity_spawner.gd
+++ entities/entity_spawner.gd
@@ -1,17 +1,17 @@
-extends StaticBody2D
+extends Node2D
 
 signal spawn_on_position(position, type)
-var enemy_scene = preload('res://creatures/human/human.tscn')
-@onready var main_node = get_parent().get_parent()
 
+@export var spawn_location: Node2D
+@export var spawnable: Node2D
 
-func _input_event(viewport: Viewport, event: InputEvent, shape_id: int) -> void:
+func _on_static_body_2d_input_event(viewport: Viewport, event: InputEvent, shape_id: int) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			print('spawning human at ', global_position)
-			var enemy = enemy_scene.instantiate()
-			main_node.add_child(enemy)
-			enemy.global_position = get_global_mouse_position()
+			var enemy = spawnable.duplicate()
+			spawn_location.add_child(enemy)
+			
 			spawn_on_position.emit(global_position, "human")
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
 			print('spawning friendly at ', global_position)
