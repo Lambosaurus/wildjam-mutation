9b1ed6b195aee73a0bd7d4912fd02f5a16df6722
Tim Lamborn <tlamborn@gmail.com>
2026-06-16 22:06:38 +1200

    Mutation application

--- creatures/mutations/mutation.gd
+++ creatures/mutations/mutation.gd
@@ -2,15 +2,15 @@ class_name Mutation
 
 extends Resource
 
-@export var behaviour: BehaviourModification
 @export var slot: Slot.Type
 @export var body_part: PackedScene
+@export var mutation_type: MutationType
 
-func modify_attributes():
-	# TODO
-	pass
+func modify_attributes(mt: MutantType):
+	if mutation_type:
+		mutation_type.apply(mt)
 
 func get_body_part():
-	if not body_part: return null
-	
+	if not body_part:
+		return null
 	return body_part.instantiate()
