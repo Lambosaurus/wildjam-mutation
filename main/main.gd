b48f4516801a80604d5c6462c01c0cd5539fea96
Tim Lamborn <tlamborn@gmail.com>
2026-06-20 20:51:20 +1200

    Add camera zoom on mousewheel

--- main/main.gd
+++ main/main.gd
@@ -4,6 +4,13 @@ extends Node2D
 @export_group("Camera")
 @export var camera_speed = 20
 @export var threat_level = 0
+@export var min_zoom: float = 0.25
+@export var max_zoom: float = 2.0
+
+@export var zoom = 1.0:
+	set(value):
+		zoom = value
+		$MainCamera.zoom = Vector2(zoom, zoom)
 
 var pause_menu = preload("res://ui/pause_menu.tscn")
 
@@ -19,5 +26,11 @@ func _process(delta):
 func update_camera():
 	var move_direction = Input.get_vector("left", "right", "up", "down")
 	if move_direction:
-		$MainCamera.position += move_direction * camera_speed
+		$MainCamera.position += move_direction * camera_speed / zoom
 	
+	if Input.is_action_just_released("scroll_up"):
+		if zoom < max_zoom:
+			zoom *= 1.1
+	elif Input.is_action_just_released("scroll_down"):
+		if zoom > min_zoom:
+			zoom /= 1.1
